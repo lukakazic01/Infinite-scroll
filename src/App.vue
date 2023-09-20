86c42d567610fa3b328b0642d57b7890b214bf60
Luka Kažić <kazicluka4@gmail.com>
2023-09-20 15:22:05 +0200

    created responsive gallery

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="text-center">
-    <p>aaa</p>
+  <div class="text-center mb-8">
+    <h1 class="text-4xl font-bold">Rick and Morty Gallery</h1>
+  </div>
+  <div class="flex flex-wrap items-center gap-y-8 justify-center" v-if="allData">
+      <div v-for="item in allData.results" :key="item.id" class="lg:basis-4/12 sm:basis-6/12 xs:basis-full flex justify-center">
+          <RickAndMortyCard :item="item" />
+      </div>
   </div>
 </template>
 
 <script setup>
+import axios from "axios";
+import {ref} from "vue";
+import RickAndMortyCard from "@/components/RickAndMortyCard.vue";
+
+const allData = ref(null);
+
+(async () => {
+   const {data} = await axios.get('https://rickandmortyapi.com/api/character')
+    allData.value = data;
+    console.log(allData.value.results)
+})()
+
 </script>
 
 <style scoped>
---
+++ src/components/RickAndMortyCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-col card">
+      <img :src="props.item.image" width="300" height="300"/>
+      <div class="p-2">
+        <p><span class="font-bold">Name:</span> {{props.item.name}}</p>
+        <p><span class="font-bold">Status:</span> {{props.item.status}}</p>
+        <p><span class="font-bold">Created:</span> {{dateFormat(props.item.created)}}</p>
+        <p><span class="font-bold">Loc:</span> {{props.item.location.name}}</p>
+      </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    item: Object,
+    required: true
+})
+
+const dateFormat = (date) => {
+    return new Date(date).toLocaleDateString('en-US')
+}
+</script>
+
+<style scoped>
+.card {
+  box-shadow: 0px 0px 20px 0px gray;
+}
+</style>
